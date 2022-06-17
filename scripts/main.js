035d636d296b8113c3f0f272f4e1c5e2cdbb7248
arkajitroy <arkajitroy11.official@gmail.com>
2022-06-17 17:56:31 +0530

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const header = document.querySelector('header')
+
+/*----------------------- Sticky Navbar ---------------------*/
+const stickyNavbar = () => {
+    header.classList.toggle('scrolled', window.pageYOffset > 0)
+}
+stickyNavbar()
+
+window.addEventListener('scroll', stickyNavbar)
+
+/*----------------------- Reveal Animation ---------------------*/
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: '60px',
+})
+
+sr.reveal('.showcase-info', { delay: 300 })
+sr.reveal('.showcase-image', { origin: 'top', delay: 400 })
